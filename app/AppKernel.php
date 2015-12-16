ede012147edff5b138fe1ab4166f190a958151a9
puterakahfi <puterakahfi@gmail.com>
2015-12-17 00:30:10 +0700

    updated

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new Cjlabs\BlogBundle\CjlabsBlogBundle(),
             new Cjlabs\CoreBundle\CjlabsCoreBundle(),
+            new Cjlabs\BlogServiceBundle\CjlabsBlogServiceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
